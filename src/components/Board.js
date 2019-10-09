5edb6cadd5c74b4dddc0fa2b461749367345fcd9
Evgueni Tchistiakov <evgueni.tchistiakov@devoteam.com>
2019-10-09 14:45:39 +0200

    logic for defining a winner + full board

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,21 +9,39 @@ export default class Board extends Component {
             currentlyPlaying: 'X'
         }
         this.handleClick = this.handleClick.bind(this)
-
     }
 
     handleClick(e) {
         const { tiles, currentlyPlaying } = this.state;    
         if (tiles[e.target.id] == null) {
             tiles[e.target.id] = currentlyPlaying === "X" ? "X" : "O";
+            if (this.checkWin(currentlyPlaying) || tiles.filter((x) => x != null).length === 9) {
+                return this.endGame();
+            }
             const nextToPlay = currentlyPlaying === "X" ? "O" : "X";
-    
             this.setState({
               tiles,
               currentlyPlaying: nextToPlay
             });
         }
+
       }
+
+      checkWin(player) {
+        const { tiles } = this.state;
+        const horizontal = [0,3,6].map(i => [i,i+1,i+2]);
+        const vertical = [0,1,2].map(i => [i,i+3,i+6]);
+        const diagonal = [[0,4,8],[2,4,6]];
+        const allwins = [].concat(horizontal).concat(vertical).concat(diagonal);
+        const result = allwins.some(indices => { 
+            return tiles[indices[0]] === player && tiles[indices[1]] === player && tiles[indices[2]] === player
+        })
+            return result;
+        }
+      
+        endGame() {
+            console.log("GAME OVER!")
+        }
 
     render() {
         return (
--- src/tests/Board.test.js
+++ src/tests/Board.test.js
@@ -5,36 +5,44 @@ import Board from '../components/Board';
 describe('<Board />', () => {
   it('should render the Board component with its title', () => {
     const wrapper = shallow(<Board />);
+
     expect(wrapper.contains(<h1>Tic Tac Toe game !</h1>)).toEqual(true);
   });
 
   it('should have in state an array of 9 null elements', () => {
     const wrapper = shallow(<Board />);
+
     expect(wrapper.state('tiles')).toEqual([null,null,null,null,null,null,null,null,null])
   });
 
   it('should set the player to X when game starts', () => {
     const wrapper = shallow(<Board />);
+
     expect(wrapper.state('currentlyPlaying')).toEqual('X');
   });
 
   it('should render the Board component with 9 Tiles', () => {
     const wrapper = shallow(<Board />);
+
     expect(wrapper.find('.tiles').children()).toHaveLength(9)
   });
 
   it('should render the 9 Tiles with unique key', () => {
     const wrapper = shallow(<Board />);
+
     const keys = [];
     wrapper.find('.tiles').children().forEach((tile) => keys.push(tile.key()))
+
     expect(new Set(keys).size).toEqual(9);
   });
 
   it('should render 9 Tiles with displayed X or O from the Board state', () => {
     const wrapper = mount(<Board />);
+
     wrapper.setState({
       tiles: ['X',null,null,null,'X',null,null,null,'O']
     });
+
     expect(wrapper.find('.tiles').children().at(0).text()).toEqual('X');
     expect(wrapper.find('.tiles').children().at(2).text()).toEqual('');
     expect(wrapper.find('.tiles').children().at(8).text()).toEqual('O');
@@ -42,22 +50,90 @@ describe('<Board />', () => {
 
   it('should prevent to replace an already clicked tile with a new label from Board currentlyPlaying info', () => {
     const wrapper = mount(<Board />);
+
     wrapper.setState({
       tiles: ['X',null,null,null,null,null,null,null,null],
       currentlyPlaying: 'O'
     });
     wrapper.find('.tiles').children().at(0).simulate('click')
+
     expect(wrapper.find('.tiles').children().at(0).text()).toEqual('X');
   });
 
   it('should replace an unclicked tile with a new label from Board currentlyPlaying info', () => {
     const wrapper = mount(<Board />);
+
     wrapper.setState({
       tiles: ['X',null,null,null,null,null,null,null,null],
       currentlyPlaying: 'O'
     });
-
     wrapper.find('.tiles').children().at(1).simulate('click')
+
     expect(wrapper.find('.tiles').children().at(1).text()).toEqual('O');
   });
+
+  it('should detect if there is a win when a column is full of X or Os', () => {
+    const wrapper = mount(<Board />);
+    const instance = wrapper.instance();
+
+    wrapper.setState({
+      tiles: ['X',null,null,'X',null,null,'X',null,null]
+    });
+
+    expect(instance.checkWin('X')).toEqual(true);
+  });
+
+  it('should detect if there is a win when a row is full of X or Os', () => {
+    const wrapper = mount(<Board />);
+    const instance = wrapper.instance();
+
+    wrapper.setState({
+      tiles: ['X','X','X',null,null,null,null,null,null]
+    });
+
+    expect(instance.checkWin('X')).toEqual(true);
+  });
+
+  it('should detect if there is a win when a diagonal or anti-diagonal is full of X or Os', () => {
+    const wrapper = mount(<Board />);
+    const instance = wrapper.instance();
+
+    wrapper.setState({
+      tiles: ['X','X','O',null,'O',null,'O',null,null]
+    });
+
+    expect(instance.checkWin('O')).toEqual(true);
+  });
+
+  it('should change the player after an empty Tile has been clicked', () => {
+    const wrapper = mount(<Board />);
+    const instance = wrapper.instance();
+
+    wrapper.setState({currentlyPlaying: 'O'});
+    wrapper.find('.tiles').children().at(2).simulate('click')
+
+    expect(instance.state).toEqual({
+        tiles: [null,null,'O',null,null,null,null,null,null],
+        currentlyPlaying: 'X'
+    })
+  });
+
+  it('should endGame when the Board has been fully clicked without changing the player', () => {
+    const wrapper = mount(<Board />);
+    const instance = wrapper.instance();
+    
+    wrapper.setState({
+        tiles: ['X','O',null,'O','X','X','O','X','O'],
+        currentlyPlaying: 'O'
+    });
+    wrapper.find('.tiles').children().at(2).simulate('click')
+
+    expect(instance.state).toEqual({
+        tiles: ['X','O','O','O','X','X','O','X','O'],
+        currentlyPlaying: 'O'
+    })
+  });
+
 });
+
+// tiles: ['X','O','O','O','X','X','O','X','O'], // tie grid
