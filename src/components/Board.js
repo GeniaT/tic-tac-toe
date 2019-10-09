fb5c334c39d429aaf92a0c1f8587c98161ed609c
Evgueni Tchistiakov <evgueni.tchistiakov@devoteam.com>
2019-10-09 08:57:06 +0200

    add initial state to Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 export default class Board extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tiles: [null,null,null,null,null,null,null,null,null],
+            currentlyPlaying: 'X'
+        }
+    }
+
     render() {
         return (
             <div>
--- src/tests/Board.test.js
+++ src/tests/Board.test.js
@@ -7,4 +7,14 @@ describe('<Board />', () => {
     const wrapper = shallow(<Board />);
     expect(wrapper.contains(<h1>Tic Tac Toe game !</h1>)).toEqual(true);
   });
+
+  it('should have in state an array of 9 null elements', () => {
+    const wrapper = shallow(<Board />);
+    expect(wrapper.state('tiles')).toEqual([null,null,null,null,null,null,null,null,null])
+  });
+
+  it('should set the player to X when game starts', () => {
+    const wrapper = shallow(<Board />);
+    expect(wrapper.state('currentlyPlaying')).toEqual('X');
+  })
 });
